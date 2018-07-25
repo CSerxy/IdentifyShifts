b0471d2fd68e0b5fb06f9de5bdda72dc1071cdb0
CSerxy <cserxy@gmail.com>
2018-07-25 15:51:15 -0400

    Update

--- preTrain/dealWith_org_dataset.py
+++ preTrain/dealWith_org_dataset.py
@@ -8,16 +8,16 @@ def main(args):
     input_file = args.input_file
     output_dir = args.output_dir
     count = 0
-    count2 = 0
+#    count2 = 0
     if not path.exists(output_dir):
         os.mkdir(output_dir)
 
     with open(input_file, "rb") as f:
         lines = f.readlines()
         for line in lines:
-            count2 += 1
-            if count2 % 10000 == 0:
-                print count2
+#            count2 += 1
+#            if count2 % 10000 == 0:
+#                print count2
             
             if line != "\n" and line.startswith("{\"abstract\": \""):
                 count += 1
@@ -31,7 +31,7 @@ def main(args):
                 w.write("\n<end>\n")
                 w.close()
 
-    print "The number of paper is: " + str(count)
+    print "The number of paper in " + input_file + " is: " + str(count)
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-i", "--input_file", dest = "input_file", help = "Input file.")
